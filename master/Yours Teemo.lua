6e8b9a6a7d932017fa28ab094a72804e4f870368
jineyne <jineyne@gmail.com>
2015-02-13 20:51:06 +0900

    Yours Teemo 1.01
    Yours Teemo Update 1.01

--- master/Yours Teemo.lua
+++ master/Yours Teemo.lua
@@ -1,6 +1,9 @@
 
+local lastAttack, lastWindUpTime, lastAttackCD = 0, 0, 0 --we initalize the variables
+	
 local ts
-local VERSION = 1.00
+local VERSION = 1.01
+local Qcasting = false
 -- rance
 
 local MyminBBox = GetDistance(myHero.minBBox)/2
@@ -11,15 +14,21 @@ function OnLoad()
 	if myHero.charName ~="Teemo" then return end
 	
 	PrintChat("Hello, Yours Teemo ver."..VERSION)
+	
     ConfigYT = scriptConfig("yours Teemo", "yoursTeemo")
 		ConfigYT:addSubMenu("combo","combo")
 			ConfigYT.combo:addParam("combohotkey","Combo Hot Key", SCRIPT_PARAM_ONKEYDOWN, false, 32)
 			ConfigYT.combo:addParam("castq", "Cast Q", SCRIPT_PARAM_ONOFF, true)
 			ConfigYT.combo:addParam("castw", "Cast W", SCRIPT_PARAM_ONOFF, true)
 			--ConfigYT.combo:addParam("castr", "Cast R", SCRIPT_PARAM_ONOFF, true)
+			
+		ConfigYT:addSubMenu("orbwalk","orbwalk")
+			ConfigYT.orbwalk:addParam("OWcombomod","Combo Mode", SCRIPT_PARAM_ONKEYDOWN, false, 32)
+			--ConfigYT.orbwalk:addParam("OWlasthit","lasthit", SCRIPT_PARAM_ONKEYDOWN, false, string.byte("X"))
+			--ConfigYT.orbwalk:addParam("OWharass","harass", SCRIPT_PARAM_ONKEYDOWN, false, string.byte("C"))
 	
 		ConfigYT:addSubMenu("harass", "harass")
-			ConfigYT.harass:addParam("AUTOQ", "killsteal Auto Q", SCRIPT_PARAM_ONKEYTOGGLE, true, string.byte("N"))
+			ConfigYT.harass:addParam("killstealQ", "killsteal Auto Q", SCRIPT_PARAM_ONKEYTOGGLE, true, string.byte("N"))
 		
 		ConfigYT:addSubMenu("draw", "draw")
 			ConfigYT.draw:addParam("aadraw", "Draw AA rance", SCRIPT_PARAM_ONOFF, true)
@@ -32,9 +41,12 @@ function OnTick()
 
 	if myHero.dead then return end
 	
-	AUTOQ()
+	killsteal()
 	COMBO()
 	OnDraw()
+	if ConfigYT.orbwalk.OWcombomod then
+		OrbWalk()
+	end
 end
 
 function COMBO()
@@ -48,7 +60,9 @@ function COMBO()
 		if (ts.target ~= nil) then
 			if ConfigYT.combo.castq then
 				if (myHero:CanUseSpell(_Q) == READY) then
+					Qcasting = true
 					CastSpell(_Q, ts.target)
+					Qcasting = false
 				end
 			end
 		end
@@ -65,8 +79,8 @@ function OnDraw()
     end
 end
 
-function AUTOQ()
-	if ConfigYT.harass.AUTOQ then
+function killsteal()
+	if ConfigYT.harass.killstealQ then
 		ts:update()
 		if (ts.target ~= nil) then
 			if (ts.target.health < 100) and (myHero:CanUseSpell(_Q) == READY) then
@@ -74,4 +88,44 @@ function AUTOQ()
 			end
 		end
 	end
+end
+
+-- orbwalk
+
+function OrbWalk()
+	ts:update() --our target is teh currently selected one(you can also use the targetselector whatever)
+	if ts.target ~=nil and GetDistance(ts.target) <= AArance then --if it valid then lets go
+		if timeToShoot() then --see code later
+			myHero:Attack(ts.target) --AA
+		elseif heroCanMove() and Qcasting == false then --if we cant attack but we can move
+			moveToCursor() --we move to cursor
+		end
+	else		
+		moveToCursor() --if the target isnt valid we jsut move to cursor
+	end
+end
+
+function OnProcessSpell(object, spell) --if smth happens
+	if object == myHero then --if we are teh owner from teh object
+		if spell.name:lower():find("attack") then --and the name is attack
+			lastAttack = GetTickCount() - GetLatency()/2 --the last attack is GetTickCount, that is the curent time - getlatenxy/2, why 2? because we need the time for the attack client-server and server-client
+			lastWindUpTime = spell.windUpTime*1000 --the lastwinduptime
+			lastAttackCD = spell.animationTime*1000 --and our "cd"
+		end 
+	end
+end
+
+function heroCanMove()
+	return (GetTickCount() + GetLatency()/2 > lastAttack + lastWindUpTime + 20) --if aa on cd, we can move
+end 
+ 
+function timeToShoot()
+	return (GetTickCount() + GetLatency()/2 > lastAttack + lastAttackCD) --if aa ready, we can shoot
+end 
+
+function moveToCursor()
+	if GetDistance(mousePos) > 10 then --well not rly much to say 
+		local moveToPos = myHero + (Vector(mousePos) - myHero):normalized()*250 --we just move 250 units in direction of our mouse
+		myHero:MoveTo(moveToPos.x, moveToPos.z) --and there we go
+	end 
 end
