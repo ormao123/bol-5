d1071b03cc08ffd85c3e0db67f7db3b80a2ae2c6
jineyne <jineyne@gmail.com>
2015-02-13 23:43:22 +0900

    fix auto update

--- Yours Teemo.lua
+++ Yours Teemo.lua
@@ -11,20 +11,20 @@ local Qdamage = { 80, 125, 170, 215, 260}
 
 local AUTO_UPDATE = true
 local UPDATE_HOST = "raw.github.com"
-local UPDATE_PATH = "/jineyne/bol/master/Your Teemo.lua".."?rand="..math.random(1,10000)
-local UPDATE_FILE_PATH = LIB_PATH.."Your Teemo.lua"
+local UPDATE_PATH = "/jineyne/bol/master/Yours Teemo.lua".."?rand="..math.random(1,10000)
+local UPDATE_FILE_PATH = LIB_PATH.."Yours Teemo.lua"
 local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
 
-local function AutoupdaterMsg(msg) print("<font color=\"#6699ff\"><b>Your Teemo:</b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
+local function AutoupdaterMsg(msg) print("<font color=\"#6699ff\"><b>Yours Teemo:</b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
 if AUTO_UPDATE then
-	local ServerData = GetWebResult(UPDATE_HOST, "/jineyne/bol/master/Your Teemo.version")
+	local ServerData = GetWebResult(UPDATE_HOST, "/jineyne/bol/master/Yours Teemo.version")
 	if ServerData then
 		ServerVersion = type(tonumber(ServerData)) == "number" and tonumber(ServerData) or nil
 		if ServerVersion then
 			if tonumber(version) < ServerVersion then
 				AutoupdaterMsg("New version available"..ServerVersion)
-				--[[AutoupdaterMsg("Updating, please don't press F9")
-				DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () AutoupdaterMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end) end, 3)]]
+				AutoupdaterMsg("Updating, please don't press F9")
+				DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () AutoupdaterMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end) end, 3)
 			else
 				AutoupdaterMsg("You have got the latest version ("..ServerVersion..")")
 			end
