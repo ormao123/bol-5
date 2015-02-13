011199ad38b2d47f4b58520739d8e2740415e983
jineyne <jineyne@gmail.com>
2015-02-13 23:13:38 +0900

    Update Yours Teemo.lua

--- Yours Teemo.lua
+++ Yours Teemo.lua
@@ -24,9 +24,9 @@ if AUTO_UPDATE then
 			if tonumber(version) < ServerVersion then
 				AutoupdaterMsg("New version available"..ServerVersion)
 				--[[AutoupdaterMsg("Updating, please don't press F9")
-				DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () AutoupdaterMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end) end, 3)
+				DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () AutoupdaterMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end) end, 3)]]
 			else
-				AutoupdaterMsg("You have got the latest version ("..ServerVersion..")")]]
+				AutoupdaterMsg("You have got the latest version ("..ServerVersion..")")
 			end
 		end
 	else
