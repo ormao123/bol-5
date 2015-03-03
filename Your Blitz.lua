90251bbfab3db69fbb6d5d598723fff33d4a597b
jineyne <jineyne@gmail.com>
2015-03-04 02:24:26 +0900

    Update

--- Your Blitz.lua
+++ Your Blitz.lua
@@ -1,6 +1,6 @@
 if myHero.charName ~= "Blitzcrank" then return end
 
-local version = 1.00
+local version = 1.01
 local AUTO_UPDATE = true
 local UPDATE_HOST = "raw.github.com"
 local UPDATE_PATH = "/jineyne/bol/master/Your Blitz.lua".."?rand="..math.random(1,10000)
@@ -30,6 +30,7 @@ require 'HPrediction'
 require "SxOrbWalk"
 
 local ts
+local player = myHero
 
 function OnLoad()
 	HPred = HPrediction()
@@ -44,7 +45,7 @@ function OnLoad()
 	Spell_Q.width['Blitzcrank'] = 70
 
   
-	ts = TargetSelector(TARGET_NEAR_MOUSE, 1000)
+	ts = TargetSelector(TARGET_MOST_AD, 1000)
 	
 	LoadMenu()
 end
@@ -52,6 +53,27 @@ end
 function OnTick()
 	Combo()
 	OnSpellcheck()
+	
+	  if GetLeastHealthAround() < Config.ag.agper then return end
+
+    -- AutoGrab
+    for _, enemy in ipairs(GetEnemyHeroes()) do
+        if ValidTarget(enemy, 1500) and Config.targets[enemy.charName] == 2 and Config.ag.useq then
+            local Pos, HitChance = HPred:GetPredict("Q", enemy, myHero)
+            
+            if hitChance >= 1 and  _GetDistanceSqr(Pos) < 925 then
+                if not CheckHeroCollision(Pos) then
+                    CastSpell(_Q, Pos.x, Pos.z)
+                    return
+                end
+            elseif hitChance >= 1 and _GetDistanceSqr(Pos) < 925 then
+                if _GetDistanceSqr(Pos) > 300 * 300 and not CheckHeroCollision(Pos) then
+                    CastSpell(_Q, Pos.x, Pos.z)
+                    return
+                end
+            end
+        end
+    end
 end
 
 function LoadMenu()
@@ -66,6 +88,10 @@ function LoadMenu()
         for i, enemy in ipairs(GetEnemyHeroes()) do
             Config.targets:addParam(enemy.charName, enemy.charName, SCRIPT_PARAM_LIST, 2, {"Don't grab", "Normal grab"})
         end
+		
+		Config:addSubMenu("Auto Grap", "ag")
+			Config.ag:addParam("useg", "Use Auto Grap", SCRIPT_PARAM_ONOFF, true)
+			Config.ag:addParam("agper", "Don't auto grab if my health < %", SCRIPT_PARAM_SLICE, 0, 0, 100)
 		
 		Config:addSubMenu("Draw", "draw")
 			Config.draw:addParam("drawqrance", "Draw Q rance", SCRIPT_PARAM_ONOFF, true)
@@ -100,6 +126,51 @@ function Combo()
 			end
 		end
 	end
+end
+
+function CheckHeroCollision(pos)
+
+    for i, enemy in ipairs(GetEnemyHeroes()) do
+        if ValidTarget(enemy) and _GetDistanceSqr(enemy) < math.pow(925 * 1.5, 2) and menu.targets[enemy.charName] == 1 then
+            local proj1, pointLine, isOnSegment = VectorPointProjectionOnLineSegment(Vector(player), pos, Vector(enemy))
+            if (_GetDistanceSqr(enemy, proj1) <= math.pow(enemy.minBBox * 2 + 70, 2)) then
+                return true
+            end
+        end
+    end
+    return false
+
+end
+
+function OnTargetInterruptable(unit, spell)
+    -- Don't grab on low health
+    if GetLeastHealthAround() < Config.ag.agper then return end
+
+    if Rready and GetDistance(unit, myHero) < 600 then
+        CastSpell(_R)
+    end
+end
+
+function OnTargetGapclosing(unit, spell)
+    -- Don't grab on low health
+    if GetLeastHealthAround() < Config.ag.agper then return end
+
+    if Qready then
+        CastSpell(_Q, unit.x, unit.z)
+    end
+end
+
+function GetLeastHealthAround()
+
+    local ph = player.health / player.maxHealth * 100
+    for i, ally in ipairs(GetAllyHeroes()) do
+        local ah = ally.health / ally.maxHealth * 100
+        if ah <= ph and not ally.dead and _GetDistance(ally) < 700 * 700 then
+            ph = ah
+        end
+    end
+    return ph
+
 end
 
 function OnSpellcheck()
